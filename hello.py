c8e77feac0e729f0176445259ef8ba0a3731ede6
adraughon <austin@radialflow.net>
2024-12-20 11:30:59 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/13evkYlWR67JFd9d77xZdbvct75Lw_POK3oa-m5G9xDU/edit?gid=0#gid=0"
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
